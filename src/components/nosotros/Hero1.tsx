ff35b22db59e4bfdb31d0c9fdc1b6305165d9151
JuanCurz <juancruzbonadeo04@gmail.comm>
2025-03-01 10:42:36 -0300

    br nosotros

--- src/components/nosotros/Hero1.tsx
+++ src/components/nosotros/Hero1.tsx
@@ -8,7 +8,7 @@ export const Hero1 = () => {
          <div className="flex flex-col md:flex-row gap-15 md:gap-0 lg:gap-20 justify-center items-center !pt-30 !pb-20  md:!my-40 fade-in">
          
             <div className="flex flex-col gap-5 justify-center items-center lg:items-start">
-                <h1 className='text-4xl font-bold text-center max-w-md md:max-w-xl' >SOMOS <span className=' text-primary'>DebonSolutions</span></h1>
+                <h1 className='text-4xl font-bold text-center' >SOMOS <br className=' lg:hidden'/><span className=' text-primary'>DebonSolutions</span></h1>
                 <p className='text-md text-center md:text-left max-w-sm'>Utilizamos tecnologías de vanguardia para transformar organizaciones,  potenciando su crecimientol.</p>
             </div>
 
