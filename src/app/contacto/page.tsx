6896543091c960f35bbae514c114a846ce98c48a
JuanCurz <juancruzbonadeo04@gmail.comm>
2025-03-06 15:16:25 -0300

    Add metadata for ContactPage with title and description

--- src/app/contacto/page.tsx
+++ src/app/contacto/page.tsx
@@ -1,5 +1,12 @@
 import { ContactUs } from "@/components/home/ContactUs";
 
+
+
+export const metadata = {
+ title: 'Contacto',
+ description: 'Esta es la página de contacto',
+};
+
 export default function ContactPage() {
   return (
     <div className="md:min-h-[75vh] !pt-0 md:!pt-25 lg:!pt-0">
